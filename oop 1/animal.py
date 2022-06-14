89d4bb0b51cba727cfc22e79293e602b4422d081
WoraphonPhonputtha <64210064@dpu.ac.th>
2022-06-14 13:54:57 +0700

    Class and Instance variable 2

---
+++ oop 1/animal.py
@@ -0,0 +1,39 @@
+class Animal:
+    animal = 'CAT'
+
+    def new_animal(self,name:str,breed:str,color:str,age:int):
+        self.name = name
+        self.breed = breed
+        self.color = color
+        self.age = age
+    
+    def print_detail(self):
+        print('---------------')
+        print(f"Name : {self.name}")
+        print(f"Animal : {self.animal}")
+        print(f"Breed : {self.breed}")
+        print(f"Color : {self.color}")
+        print(f"Age : {self.age}")
+
+if __name__ == "__main__":
+    Animal.animal = 'FISH'
+    ula = Animal()
+    ula.animal = 'DOG'
+    ula.new_animal('Ula', 'Scottish', 'White', 1)
+    ula.print_detail()
+    
+    drac = Animal()
+    drac.new_animal('Drac', 'Scottish', 'White', 1)
+    drac.print_detail()
+    drac.breed = 'Catfish'
+    drac.print_detail()
+
+    #เรียกดูข้อมูลของ Object ผ่านชื่อ Class
+    Animal.print_detail(ula)
+    Animal.print_detail(drac)
+
+    #เรียกดู Class Variable ทั้งหมด
+    print(f"{Animal.__dict__}")
+
+    #เรียกดู Instance Variable ทั้งหมด
+    print(f"{ula.__dict__}")
