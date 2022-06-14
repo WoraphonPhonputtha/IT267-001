dd876b8be7d7f2941aa9ab96211e993dc4e4233e
WoraphonPhonputtha <64210064@dpu.ac.th>
2022-06-14 11:45:58 +0700

    Class and Instance Variable

--- oop 1/person.py
+++ oop 1/person.py
@@ -1,4 +1,5 @@
 class Person:
+    country = 'Thailand'
     def __init__(self,name,gender,profession,hours) -> None:
         self.name = name
         self.gender = gender
@@ -18,9 +19,26 @@ jessa = Person("Jessa", "Male", "Software Engineer", 10)
 jessa.show()
 jessa.work()
 jessa.study()
+
 Jon = Person("Jon", "Male", "Doctor", 15)
 Jon.show()
 Jon.work()
 Jon.study()
+
 Lisa = Person('Lalisa', 'Female', 'Korean Singer', 13)
 Lisa.work()
+
+print(f'Class Variable : {Person.country}')
+print(f'Instance Variable : {Lisa.country}')
+
+Lisa.country = 'Korea'
+print('---------------')
+print(f'Class Variable : {Person.country}')
+print(f'Instance Variable : {Lisa.country}')
+print(f'Instance Variable : {Jon.country}')
+
+Person.country = 'England'
+print('///////////////')
+print(f'Class Variable : {Person.country}')
+print(f'Instance Variable : {Lisa.country}')
+print(f'Instance Variable : {Jon.country}')
