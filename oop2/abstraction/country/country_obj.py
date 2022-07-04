1b32e331b8398a98ccf30fc990e76b1f37319974
WoraphonPhonputtha <64210064@dpu.ac.th>
2022-07-04 14:11:02 +0700

    country

---
+++ oop2/abstraction/country/country_obj.py
@@ -0,0 +1,10 @@
+from country_subclass import *
+
+e = Europe('France', 65561783)
+e.capital = 'Paris'
+e.location = '46.2276° N,2.2137° E'
+e.show_detail()
+
+a = Asia('Singapore', 5942060)
+a.gdp = 323907234412
+a.show_detail()
